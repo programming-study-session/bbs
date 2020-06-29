9f52203970545442dfbf223e675580a43a630ba3
hosoe5039 <hosoe.adgjmptz@gmail.com>
2020-06-29 23:03:43 +0900

    (1)users_listから読み出す処理をメソッド化 (2)comment_listのデータ数をカウントする処理をメソッド化 (3)コメント投稿処理をメソッド化 (4)thread_list読み出し処理をメソッド化 (5)comment_list読み出し処理をメソッド化

--- src/info/java/study/CommentEdit.java
+++ src/info/java/study/CommentEdit.java
@@ -5,6 +5,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -21,15 +23,9 @@ public class CommentEdit extends HttpServlet {
 			throws ServletException, IOException {
 		// DB関連の初期設定
 		Connection conn = null;
-		PreparedStatement comment_insert_state = null;
-		PreparedStatement Contributor_name_state = null;
-		PreparedStatement thread_read_state = null;
-		PreparedStatement comment_read_state = null;
-		PreparedStatement thread_info_state = null;
 		ResultSet thread_result_set = null;
-		ResultSet Contributor_name_result_set =null;
+		ResultSet user_info_result_set =null;
 		ResultSet comment_result_set = null;
-		ResultSet thread_info_result_set = null;
 
 		// 文字コードの設定
 		request.setCharacterEncoding("Windows-31J");
@@ -57,168 +53,70 @@ public class CommentEdit extends HttpServlet {
 				conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/java_study?serverTimezone=UTC&useSSL=false",
 						"root", "searchman");
 
-				//投稿者の名前を取得-------------------------------------------------------------------------------------------
-				// sql文作成
-				String sql_Contributor_name = "select * from users_list where id =" + user_id + ";";
-				System.out.println(sql_Contributor_name);
+				//投稿者の名前を取得
+				user_info_result_set = readUsersDb(conn, user_id);
+				user_info_result_set.next();
+				String Contributor_name = user_info_result_set.getString(2);
 
-				//sql実行→実行結果を代入
-				Contributor_name_state = conn.prepareStatement(new String(sql_Contributor_name));
-				Contributor_name_state.execute();
-				Contributor_name_result_set = Contributor_name_state.executeQuery();
-
-				//実行結果を取り出してContributor_nameクラスに代入
-
-				String Contributor_name = null;
-
-				while(Contributor_name_result_set.next()) {
-					Contributor_name = Contributor_name_result_set.getString(2);
-				}
-				////スレッドの投稿数を取得-------------------------------------------------------------------------------------
-				///*			//スレッドの投稿数を取得
-				//			String sql_thread_count = "select count(*) from comment_list where thread_id =";
-				//				sql_thread_count = sql_thread_count + thread_id + ";";
-				//
-				//			// sql文を表示
-				//			System.out.println(sql_thread_count);
-				//
-				//			// sql文実行準備
-				//			thread_count_state = conn.prepareStatement(new String(sql_thread_count));
-				//
-				//			// sql文実行
-				//			thread_count_state.execute();
-				//
-				//			// 実行結果を、ResultSetクラスに代入
-				//			thread_count_result_set =thread_count_state.executeQuery();
-				//
-				//			//実行結果を取り出してint型の変数に代入する
-				//			int comment_no_result = thread_count_state.getInt(thread_id);
-				//
-				//			System.out.println(comment_no_result);
-				//			*/
-				//comment_noが必要であるため投稿数の取得---------------------------------------------------------------------
-				//sql文作成の準備 (コメントの読み出し)
-				String sql_comment_read = "select * from comment_list where thread_id =" + thread_id + ";";
-
-				// sql文を表示 (コメントの読み出し)
-				System.out.println(sql_comment_read);
-
-				// sql文実行準備 (コメントの読み出し)
-				comment_read_state = conn.prepareStatement(new String(sql_comment_read));
-
-				// sql文実行 (コメントの読み出し)
-				comment_read_state.execute();
-
-				// 実行結果を、ResultSetクラスに代入 (コメントの読み出し)
-				comment_result_set = comment_read_state.executeQuery();
-
-				//現在のコメント計測する変数の初期化
-				int comment_no_result = 0;
-
-				//繰り返し処理によって現在の投稿数の取得
-				while(comment_result_set.next()) {
-					comment_no_result++;
-				}
-
-				//割り当てるcomment_noを設定(comment_no=現在の投稿数+1)
+				//投稿対象のスレッドのコメント数を取得
+				comment_result_set =countCommentDb(conn ,thread_id);
+				comment_result_set.next();
+				int comment_no_result = comment_result_set.getInt(1);
+				//テーブルcommnet_listにinsertするcommnet_noにする
 				comment_no_result++;
-
-				//comment_noを文字列型に変更
 				String comment_no_set = Integer.toString(comment_no_result);
 
-				//スレッドへの投稿処理---------------------------------------------------------------------------------
+				//スレッドへの投稿処理
+				editComment(conn,thread_id , comment_no_set , Contributor_name , comment);
 
-				// sql文 の作成（comment_noから）
-				String sql_comment_insert = "insert into comment_list(thread_id,comment_no,name,comment,time)";
-				sql_comment_insert = sql_comment_insert + "values"+ "(\"" + thread_id + "\"" + ',' + "\"" + comment_no_set + "\"" + ',' + "\"" + Contributor_name + "\"" + ',' + "\"" + comment + "\",now());";
-
-				// sql文を表示
-				System.out.println(sql_comment_insert);
-
-				// sql文実行準備
-				comment_insert_state = conn.prepareStatement(new String(sql_comment_insert));
-
-				// sql文実行
-				comment_insert_state.execute();
-
-				//スレッドの一覧を表示------------------------------------------------------------------------
-				String sql_thread_read = "select * from thread_list;";
-
-				// sql文を表示
-				System.out.println(sql_thread_read);
-
-
-				thread_read_state = conn.prepareStatement(new String(sql_thread_read));
-
-				// sql文実行
-				thread_read_state.execute();
-
-				// 実行結果を、ResultSetクラスに代入
-				thread_result_set = thread_read_state.executeQuery();
-
-
-				// 遷移ページへ、引渡し（Attributeで追加する）
+				//スレッドの一覧を表示
+				thread_result_set = readThreadFromDb(conn);
 				request.setAttribute("thread_kekka", thread_result_set);
 
 
-				//対応スレッド情報の表示--------------------------------------------------------------------------------------
-				// sql文作成の準備 (コメントの読み出し)
-				sql_comment_read = "select * from comment_list where thread_id =" + thread_id + ";";
-
-				// sql文を表示 (コメントの読み出し)
-				System.out.println(sql_comment_read);
-
-				// sql文実行準備 (コメントの読み出し)
-				comment_read_state = conn.prepareStatement(new String(sql_comment_read));
-
-				// sql文実行 (コメントの読み出し)
-				comment_read_state.execute();
-
-				// 実行結果を、ResultSetクラスに代入 (コメントの読み出し)
-				comment_result_set = comment_read_state.executeQuery();
-
-				// 遷移ページへ、引渡し（Attributeで追加する） (コメントの読み出し)
+				//対応スレッド情報の表示
+				comment_result_set = readCommentFromDb(conn,thread_id);
 				request.setAttribute("comment_kekka", comment_result_set);
-				//書き込み可能なスレッド情報取得---------------------------------------------------------------
-				//sql文作成の準備
-				String sql_thread_info = "select * from thread_list where thread_id =" + thread_id + ";";
 
-				// sql文を表示
-				System.out.println(sql_thread_info);
+				//書き込み可能なスレッドタイトル情報取得
+				String currentThreadId = request.getParameter("thread_id");
+				HashMap<String,String> threadTitleMap = new HashMap<String,String>();
 
-				// sql文実行準備
-				thread_info_state = conn.prepareStatement(new String(sql_thread_info));
+				String threadId;
+				String threadTitle;
 
-				// sql文実行
-				thread_info_state.execute();
+				while(thread_result_set.next()){
+				  threadId = thread_result_set.getString(1);
+				  threadTitle = thread_result_set.getString(2);
 
-				// 実行結果を、ResultSetクラスに代入
-				thread_info_result_set = thread_info_state.executeQuery();
+				  threadTitleMap.put(threadId, threadTitle);
+				}
 
-				// 遷移ページへ、引渡し（Attributeで追加する）
-				request.setAttribute("thread_info_kekka", thread_info_result_set);
-				//--------------------------------------------------------------------------------------
+				thread_result_set.beforeFirst();
+
+				String thread_title_result = threadTitleMap.get(currentThreadId);
+				request.setAttribute("thread_title", thread_title_result);
+
 				// search.jspへ遷移
 				request.getRequestDispatcher("/thread2.jsp").forward(request, response);
 
 				// 使用したオブジェクトを終了させる
 				conn.close();
-				comment_insert_state.close();
-				thread_read_state.close();
-				comment_read_state.close();
-				thread_info_state.close();
+				//thread_read_state.close();
+				//comment_read_state.close();
 				thread_result_set.close();
 				comment_result_set.close();
-				thread_info_result_set.close();
 
 			} catch (Exception e) {
-				response.sendRedirect("\"http://localhost:8090/SkillShare/Eroor.jsp");
+				e.printStackTrace();
+				response.sendRedirect("http://localhost:8090/SkillShare/Eroor.jsp");
 
 			} finally {
 				try {
 					// 念のため、finallyでDBとの接続を切断しておく
 					conn.close();
 				} catch (Exception e) {
+					e.printStackTrace();
 				  }
 			}
 		}
@@ -228,4 +126,68 @@ public class CommentEdit extends HttpServlet {
 		}
 
 	}
+
+	private ResultSet readUsersDb(Connection conn, String user_id) throws SQLException {
+		//ログインに必要なユーザー情報をusers_listから読み出し
+		String sql_users_info = "select * from users_list where id =" + user_id + ";";
+		System.out.println(sql_users_info);
+		PreparedStatement user_info_state =conn.prepareStatement(new String(sql_users_info));
+		user_info_state.execute();
+		return user_info_state.executeQuery();
+	}
+
+	private ResultSet countCommentDb(Connection conn, String thread_id) throws SQLException {
+		//ログインに必要なユーザー情報をusers_listから読み出し
+		String sql_comment_count = "select count(*) from comment_list where thread_id =" + thread_id + ";";
+		System.out.println(sql_comment_count);
+		PreparedStatement comment_count_state = conn.prepareStatement(new String(sql_comment_count));
+		comment_count_state.execute();
+		return comment_count_state.executeQuery();
+	}
+
+	private static void editComment(Connection conn, String thread_id, String comment_no_set, String Contributor_name,String comment) throws SQLException {
+		String sql_comment_insert = "insert into comment_list(thread_id,comment_no,name,comment,time)";
+		sql_comment_insert = sql_comment_insert + "values"+ "(\"" + thread_id + "\"" + ',' + "\"" + comment_no_set + "\"" + ',' + "\"" + Contributor_name + "\"" + ',' + "\"" + comment + "\",now());";
+		System.out.println(sql_comment_insert);
+		PreparedStatement comment_insert_state = conn.prepareStatement(new String(sql_comment_insert));
+		comment_insert_state.execute();
+		comment_insert_state.close();
+	}
+
+	private ResultSet readThreadFromDb(Connection conn) throws SQLException {
+
+		// sql文作成の準備 (スレッドの読み出し)
+		String sql_thread_read = "select * from thread_list;";
+
+		// (スレッドの読み出し)
+		System.out.println(sql_thread_read);
+
+		// sql文実行準備 (スレッドの読み出し)
+		PreparedStatement thread_read_state = conn.prepareStatement(new String(sql_thread_read));
+
+		// sql文実行 (スレッドの読み出し)
+		thread_read_state.execute();
+
+		// 実行結果を、Resulthread_result_setクラスに代入 (スレッドの読み出し)
+		return thread_read_state.executeQuery();
+
+	}
+
+	private ResultSet readCommentFromDb(Connection conn, String thread_id) throws SQLException {
+		// sql文作成の準備 (コメントの読み出し)
+		String sql_comment_read = "select * from comment_list where thread_id =" + thread_id + ";";
+
+		// sql文を表示 (コメントの読み出し)
+		System.out.println(sql_comment_read);
+
+		// sql文実行準備 (コメントの読み出し)
+		PreparedStatement comment_read_state = conn.prepareStatement(new String(sql_comment_read));
+
+		// sql文実行 (コメントの読み出し)
+		comment_read_state.execute();
+
+		return comment_read_state.executeQuery();
+	}
+
+
 }
