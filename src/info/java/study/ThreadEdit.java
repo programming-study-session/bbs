86743161845bc70bf7efb53c298ba7ef9b74eaa4
hosoe5039 <hosoe.adgjmptz@gmail.com>
2020-06-29 23:04:58 +0900

    (1)users_listから読み出す処理をメソッド化 (2)スレッド作成処理をメソッド化 (3)thread_list読み出し処理をメソッド化

--- src/info/java/study/ThreadEdit.java
+++ src/info/java/study/ThreadEdit.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -23,13 +24,8 @@ public class ThreadEdit extends HttpServlet {
 
 		// DB関連の初期設定
 		Connection conn = null;
-		PreparedStatement thread_creator_state = null;
-		PreparedStatement thread_insert_state = null;
-		PreparedStatement thread_read_state = null;
-		PreparedStatement thread_info_state = null;
 		ResultSet thread_creator_result_set = null;
 		ResultSet thread_result_set = null;
-		ResultSet thread_info_result_set = null;
 
 		// 文字コードの設定
 		request.setCharacterEncoding("Windows-31J");
@@ -56,15 +52,9 @@ public class ThreadEdit extends HttpServlet {
 						"jdbc:mysql://localhost:3306/java_study?serverTimezone=UTC&useSSL=false",
 						"root", "searchman");
 
-//スレッド作成者の名前をuses_listから読み出し-------------------------------------------------------------------------
-				//sql文の作成
-				String sql_thread_creator = "select * from users_list where id =" + user_id + ";";
-				System.out.println(sql_thread_creator);
 
-				//
-				thread_creator_state = conn.prepareStatement(new String(sql_thread_creator));
-				thread_creator_state.execute();
-				thread_creator_result_set = thread_creator_state.executeQuery();
+				//スレッド作成者の名前をuses_listから読み出し
+				thread_creator_result_set = readUsersDb(conn, user_id);
 
 				String thread_creator = null;
 
@@ -73,71 +63,34 @@ public class ThreadEdit extends HttpServlet {
 				}
 				System.out.println(thread_creator);
 
-				//スレッドの作成処理------------------------------------------------------------------------
-				// sql文 の作成
-				String sql_thread_insert = "insert into thread_list(thread_title,name,time)";
-				sql_thread_insert = sql_thread_insert + "values" + "(\"" + topic + "\",\"" + thread_creator + "\","
-						+ "now()" + ");";
+				//スレッドの作成処理
+				editThread(conn,topic,thread_creator);
 
-				// sql文を表示
-				System.out.println(sql_thread_insert);
 
-				// sql文実行準備
-				thread_insert_state = conn.prepareStatement(new String(sql_thread_insert));
-
-				// sql文実行
-				thread_insert_state.execute();
-
-				//スレッドの一覧を表示----------------------------------------------------------------------
-				// sql文 の作成
-				String sql_thread_read = "select * from thread_list;";
-
-				//sql文を表示
-				System.out.println(sql_thread_read);
-
-				thread_read_state = conn.prepareStatement(new String(sql_thread_read));
-
-				// sql文実行
-				thread_read_state.execute();
-
-				// 実行結果を、ResultSetクラスに代入
-				thread_result_set = thread_read_state.executeQuery();
-
-				// 遷移ページへ、引渡し（Attributeで追加する）
+				//スレッド読み出し
+				thread_result_set = readThreadFromDb(conn);
 				request.setAttribute("thread_kekka", thread_result_set);
 
-				//対応スレッド情報の表示-----------------------------------------------------------------------------------
-				//sql文作成の準備
-				String sql_thread_info = "select thread_id, thread_title from thread_list where thread_id=(select max(thread_id) from thread_list);";
+				//スレッドタイトルとスレッドIDを取得
+				thread_result_set.last();
+				String thread_id_result = thread_result_set.getString(1);
+				String thread_title_result = thread_result_set.getString(2);
+				thread_result_set.beforeFirst();
 
-				// sql文を表示
-				System.out.println(sql_thread_info);
-
-				// sql文実行準備
-				thread_info_state = conn.prepareStatement(new String(sql_thread_info));
-
-				// sql文実行
-				thread_info_state.execute();
-
-				// 実行結果を、ResultSetクラスに代入
-				thread_info_result_set = thread_info_state.executeQuery();
-
-				// 遷移ページへ、引渡し（Attributeで追加する）
-				request.setAttribute("thread_info_kekka", thread_info_result_set);
-				//-------------------------------------------------------------------------------------
+				request.setAttribute("thread_title", thread_title_result);
+				request.setAttribute("thread_id_set", thread_id_result);
 
 				// search.jspへ遷移
 				request.getRequestDispatcher("/thread2.jsp").forward(request, response);
 
 				// 使用したオブジェクトを終了させる
 				conn.close();
-				thread_insert_state.close();
-				thread_read_state.close();
-				thread_info_state.close();
+				//thread_read_state.close();
+				//thread_info_state.close();
 				thread_result_set.close();
-				thread_info_result_set.close();
 
 			} catch (Exception e) {
+				e.printStackTrace();
 				response.sendRedirect("\"http://localhost:8090/SkillShare/Eroor.jsp");
 
 			} finally {
@@ -145,6 +98,7 @@ public class ThreadEdit extends HttpServlet {
 					// 念のため、finallyでDBとの接続を切断しておく
 					conn.close();
 				} catch (Exception e) {
+					e.printStackTrace();
 				}
 			}
 		}
@@ -152,4 +106,37 @@ public class ThreadEdit extends HttpServlet {
 			response.sendRedirect("http://localhost:8090/SkillShare/Not_Enter_Warning.jsp");
 		}
 	}
+	//スレッド作成者の名前をuses_listから読み出し----------------------------------------------------------------------
+	private ResultSet readUsersDb(Connection conn, String user_id) throws SQLException {
+		String sql_thread_creator = "select * from users_list where id =" + user_id + ";";
+		System.out.println(sql_thread_creator);
+
+		PreparedStatement thread_creator_state = conn.prepareStatement(new String(sql_thread_creator));
+		thread_creator_state.execute();
+		return thread_creator_state.executeQuery();
+	}
+
+	//スレッドへの投稿処理---------------------------------------------------------------------------------------------
+	private static void editThread(Connection conn, String topic,String thread_creator) throws SQLException {
+		String sql_thread_insert = "insert into thread_list(thread_title,name,time)";
+		sql_thread_insert = sql_thread_insert + "values" + "(\"" + topic + "\",\"" + thread_creator + "\","
+				+ "now()" + ");";
+
+		System.out.println(sql_thread_insert);
+		PreparedStatement thread_insert_state = conn.prepareStatement(new String(sql_thread_insert));
+		thread_insert_state.execute();
+		thread_insert_state.close();
+	}
+
+	//スレッドの読み出し処理---------------------------------------------------------------------------------------------
+	private ResultSet readThreadFromDb(Connection conn) throws SQLException {
+
+		String sql_thread_read = "select * from thread_list;";
+		System.out.println(sql_thread_read);
+		PreparedStatement thread_read_state = conn.prepareStatement(new String(sql_thread_read));
+		thread_read_state.execute();
+		return thread_read_state.executeQuery();
+
+	}
 }
+
